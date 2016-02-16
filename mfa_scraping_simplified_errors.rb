f14ec29b54b7db9016ea57a84c2518a24d709fe8
Sam Berkowitz <skberkowitz@gmail.com>
2016-02-16 00:40:35 -0800

    tidied up urls to be appended to csv, created simplified errors version of script

--- mfa_scraping_simplified_errors.rb
+++ mfa_scraping_simplified_errors.rb
@@ -6,7 +6,7 @@ start_time = Time.now
 puts "\nStarted scraping at #{Time.now}"
 
 # creates csv doc and adds four relevant column names:
-CSV.open("mfa_press_confs.csv", "wb") do |csv|
+CSV.open("mfa_press_confs_simplified_errors.csv", "wb") do |csv|
   column_titles = %w(title date body url)
   csv << column_titles
 end
@@ -33,7 +33,7 @@ while keepgoing
     press_conf[:body] = press_conf_doc.at_css('#News_Body_Txt_A').text
     press_conf[:url] = press_conf_url
 
-    CSV.open("mfa_press_confs.csv", "ab") do |csv|
+    CSV.open("mfa_press_confs_simplified_errors.csv", "ab") do |csv|
       csv << press_conf.values
     end
 
@@ -47,34 +47,18 @@ while keepgoing
     index_doc = Nokogiri::HTML(open(index_url))
     puts "\nNow scraping links from index page #{index_idx}."
   rescue OpenURI::HTTPError => error
-    if error.message.to_i == 502
-      puts "\nHit a 502 (Bad Gateway) error. Will retry scraping this URL in 120 seconds, at #{Time.now}."
-      sleep 120
-      begin
-        index_doc = Nokogiri::HTML(open(index_url))
-      rescue OpenURI::HTTPError => error2
-        puts "\nAfter an initial 502 error, slept 120 seconds and retried same URL.\nHowever, there was another error.\nThis time, it was '#{error2.message}'"
-        keepgoing = false
-      end
-    elsif error.message.to_i == 403
-      puts "\nHit a 403 (Forbidden) error. Possibly banned from the site now."
-    elsif error.message.to_i == 404
+    if error.message.to_i == 404
       keepgoing = false
       puts "\nHit a 404 (Not Found) error - either there was a URL issue, or scraping is complete!"
     else
-      keepgoing = false
-      puts "\nThere was an unexpected error: '#{error.message}'. Uh oh!"
+      puts "\nSleeping for 60 sec to deal with #{error.message}"
+      sleep 60
+      index_doc = Nokogiri::HTML(open(index_url))
     end
-
-    # need to account for 504 errors, apparently:
-    # /Users/skberkowitz/.rbenv/versions/2.1.2/lib/ruby/2.1.0/open-uri.rb:353:in `open_http': 504 Gateway Time-out (OpenURI::HTTPError)
-    end
+  end
 end
 
 puts "\nScraping took #{(Time.now - start_time).to_i} seconds."
-
-# p "\nAll press conferences scraped and appended to mfa_press_confs.csv"
-
 
 
 # http://stackoverflow.com/questions/17325792/array-of-hashes-to-csv-file
